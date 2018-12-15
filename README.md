bd4be1696d5dddb82090d033d8b2ffbd69c3ba3a
Messivânio Galvão <messivanio.galvao@gmail.com>
2018-12-14 23:04:19 -0200

    Install

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
-My Dotfiles
+
+My [dotfiles](https://dotfiles.github.io/).
+
+## Install
+
+    curl -s https://raw.githubusercontent.com/messivanio/dotfiles/master/install/install-dotfiles.sh | sh
+    source ~/dotfiles/bash/.bashrc
+
+
--- install/install-dotfiles.sh
+++ install/install-dotfiles.sh
@@ -2,6 +2,8 @@ sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update -y
 sudo apt-get install -y git
 
+sudo locale-gen pt_BR pt_BR.UTF-8
+
 cd ~ && rm -rf ~/dotfiles && git clone https://github.com/messivanio/dotfiles.git
 grep 'source ~/dotfiles/bash/.bashrc' ~/.bashrc || echo 'source ~/dotfiles/bash/.bashrc' >> ~/.bashrc
 
