883b2a99044aa2ef80d91a9ed23e7f56562990f0
Messivânio Galvão (Messi) <messipg@trt3.jus.br>
2019-11-11 16:44:21 -0300

    Install golang

---
+++ install/install-golang.sh
@@ -0,0 +1,9 @@
+GOLANG_VERSION=1.13.4
+
+cd /tmp
+wget https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz
+
+mkdir -p ~/code/go/src
+mkdir -p ~/code/go/pkg
+mkdir -p ~/code/go/bin
