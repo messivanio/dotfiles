925b1bf3e2d05856cc8d3bc292bc5c039cfc2205
Messivânio Galvão (Messi) <messipg@trt3.jus.br>
2019-11-11 16:48:39 -0300

    Remove old golang before install

--- install/install-golang.sh
+++ install/install-golang.sh
@@ -2,6 +2,7 @@ GOLANG_VERSION=1.13.4
 
 cd /tmp
 wget https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go/
 sudo tar -C /usr/local -xzf go${GOLANG_VERSION}.linux-amd64.tar.gz
 
 mkdir -p ~/code/go/src
