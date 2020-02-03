2ad937bc5d3f4dac2c633171d501f873b1881745
Messivânio Galvão <messipg@trt3.jus.br>
2020-02-03 10:07:23 -0300

    install ansible 2.7.12

--- install/install-ansible.sh
+++ install/install-ansible.sh
@@ -1,3 +1,3 @@
-sudo apt install -y python2.7 python-pip redis
-sudo pip install 'ansible==2.7.4.0'
+sudo apt install -y python3 python3-pip redis
+sudo pip3 install 'ansible==2.7.12'
 #pip install 'redis'
