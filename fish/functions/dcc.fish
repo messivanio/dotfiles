378670563705a2b46904ea9bded3f0bcfd927f5a
Messivânio Galvão <messipg@trt3.jus.br>
2021-07-17 12:29:28 -0300

    Fix dcc

--- fish/functions/dcc.fish
+++ fish/functions/dcc.fish
@@ -1,5 +1,19 @@
 function dcc
-	docker-compose down -v --remove-orphans
-	docker-compose up --build -d
-	docker-compose logs -f
+  if $args
+    docker-compose rm --force --stop $argv 
+  else
+    docker-compose down -v--remove-orphans
+  end
+  
+	docker-compose build $argv
+	docker-compose up --remove-orphans --build -d $argv
+
+	if test $CMD_DURATION
+        set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+        set color -dim normal
+        printf "it took "
+        printf $duration
+    end
+
+	docker-compose logs -f $argv
 end
