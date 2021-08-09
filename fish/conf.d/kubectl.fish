06f58850f5394720eaa3ef5af23c3a764e1e309b
Messivânio Galvão <messipg@trt3.jus.br>
2021-08-09 13:52:34 -0300

    Fix kubectl .k8s-clusters file

--- fish/conf.d/kubectl.fish
+++ fish/conf.d/kubectl.fish
@@ -5,8 +5,8 @@ function kubectl --description "A safer kubectl"
   if contains $kube_args $unsafe_commands
     set CURRENT_CLUSTER (command kubectl config view --template='{{ range .contexts }}{{ if eq .name "'(command kubectl config current-context)'" }}{{ .context.cluster }}{{ end }}{{ end }}')
     set FILE ".k8s-clusters"
-    read -x FILE_CONTENT < $FILE
     if test -e $FILE
+      read -x FILE_CONTENT < $FILE
       if not string match -r "K8S_CLUSTERS=\"$CURRENT_CLUSTER\"" $FILE_CONTENT
         echo "Current k8s Cluster: $CURRENT_CLUSTER"
         echo "K8S Target Clusters (from .k8s-clusters file): $FILE_CONTENT"
