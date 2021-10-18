bee41ca7cd60332202f303a0428c58f7f6e31ba2
Messivânio Galvão <messivanio.galvao@gmail.com>
2021-10-18 09:34:31 -0300

    Dont try to update uninstalled apps

--- fish/functions/update-everything.fish
+++ fish/functions/update-everything.fish
@@ -1,9 +1,20 @@
 function update-everything
-  sudo apt-get update -y
-  sudo apt-get upgrade -y --fix-broken
-  sudo apt-get dist-upgrade -y
-  sudo apt autoremove -y
-  asdf update
-  asdf plugin update --all
-  brew upgrade
+  if type -qs apt-get
+    sudo apt-get update -y
+    sudo apt-get upgrade -y --fix-broken
+    sudo apt-get dist-upgrade -y
+  end
+
+  if type -qs apt
+    sudo apt autoremove -y
+  end
+
+  if type -qs asdf
+    asdf update
+    asdf plugin update --all
+  end
+
+  if type -qs brew
+    brew upgrade
+  end
 end
