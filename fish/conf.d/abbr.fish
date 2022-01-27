dfa16ed0ee66407432ab6e8989d5ecc78c44cb8b
Messivânio Galvão <messipg@trt3.jus.br>
2022-01-27 12:28:32 -0300

    amp & zellij

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,8 +2,8 @@ abbr .. cd ..
 abbr ... cd ../..
 abbr .... cd ../../..
 
-abbr a cd ~/code/agora
-abbr an ansible 
+abbr a amp
+abbr an ansible
 abbr ap ansible-playbook
 abbr be bundle exec
 abbr bec bundle exec cucumber
@@ -14,11 +14,11 @@ abbr dc docker-compose
 abbr dce docker-compose exec
 abbr de docker exec -it
 
-abbr e exit 
+abbr e exit
 
-abbr g git 
+abbr g git
 abbr ga git add
-abbr gcm git commit -m 
+abbr gcm git commit -m
 abbr gco git checkout
 abbr gd git diff
 abbr glances glances --fs-free-space
@@ -38,6 +38,9 @@ abbr kl kubectl logs -f
 abbr l ls -lha
 abbr log_error grep -i "erro\|caused"
 abbr log_warn_and_error grep -i "erro\|warn\|caused"
+
+abbr n cd ~/code/agora
+
 abbr requirements-install ansible galaxy install -r requirements.txt --force
 abbr s ssh
 abbr sai sudo apt install
@@ -46,3 +49,5 @@ abbr subl subl --add
 abbr v vagrant
 abbr vd vagrant destroy
 abbr vu vagrant up
+
+abbr z zellij
