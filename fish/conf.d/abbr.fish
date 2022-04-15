a16ec5d18763b615bad959b19c1afbda209cc702
Messivânio Galvão <messivanio.galvao@gmail.com>
2022-04-15 17:21:23 -0300

    fix abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,7 +2,7 @@ abbr .. cd ..
 abbr ... cd ../..
 abbr .... cd ../../..
 
-abbr a amp
+abbr a cd ~/code/agora
 abbr an ansible
 abbr ap ansible-playbook
 abbr be bundle exec
@@ -33,22 +33,17 @@ abbr k kubectl
 abbr ka kubectl apply -f
 abbr kak kubectl apply -k
 abbr kdk kubectl delete -k
-abbr kl kubectl logs -f
 
 abbr l ls -lha
 abbr log_error grep -i "erro\|caused"
 abbr log_warn_and_error grep -i "erro\|warn\|caused"
 
-abbr n cd ~/code/agora
+abbr n nnn -de
 
 abbr r rails
 abbr requirements-install ansible galaxy install -r requirements.txt --force
 abbr s ssh
-abbr sai sudo apt install
-abbr subl subl --add
 
 abbr v vagrant
 abbr vd vagrant destroy
 abbr vu vagrant up
-
-abbr z zellij
