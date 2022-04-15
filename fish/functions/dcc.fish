63252916977a7579d9bc95425769fdbac698df01
Messivânio Galvão <messivanio.galvao@gmail.com>
2022-04-15 17:37:24 -0300

    fix indentation

--- fish/functions/dcc.fish
+++ fish/functions/dcc.fish
@@ -11,11 +11,11 @@ function dcc
 	docker-compose up --remove-orphans --build -d $argv
 
 	if test $CMD_DURATION
-        set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
-        set color -dim normal
-        printf "it took "
-        printf $duration
-    end
+    set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+    set color -dim normal
+    printf "it took "
+    printf $duration
+  end
 
 	docker-compose logs -f $argv
 end
